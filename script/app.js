9378de91e9c38b65b6e65d13585d782304a79214
SebasttianM <sebasttianvmarin@gmail.com>
2022-02-18 17:11:11 -0500

    model1

--- script/app.js
+++ script/app.js
@@ -4,23 +4,25 @@ let popular = document.getElementById("popular").value;
 let anime = document.getElementById("anime").value;
 let crime = document.getElementById("crime").value;
 
-const getMovies = (url) => {
-  const showMovie = document.querySelector("videosCard");
-  showMovie.innerHTML = "";
-  fetch(url)
-  .then((resp) => resp.json())
-  .then((data) => {
+
+const getMovies = async (url) => {
+  const response = await fetch(url);
+  showMovie.innerHTML = ""
+  .then(response => response.json())
+  .then(data => {
     data.forEach(movie => {
-      const { image } = movie;
+      const { image } = movie
       showMovie.innerHTML += `
 			<div class='popularMovies'>
-				<img src="${image}" class="movieReel" alt="pop">
-			`;
-		console.log(data)
+				<img src="${image}" class="..." alt="...">
+      </div>  
+			`
+      card.appendChild("showMovie")
     })
   })
 }
+const cardFilled = await request("http://localhost:4000/movies")
 
-getMovies.addEventListener("click", () =>
-  getMovies("http://localhost:4005/movies")
-);
+// getMovies.addEventListener("", () =>
+//   getMovies("http://localhost:4005/movies")
+// );
