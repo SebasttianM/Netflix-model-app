5ea60056ebf1cb849926dfec670be40ccd8f073f
SebasttianM <sebasttianvmarin@gmail.com>
2022-02-19 00:51:35 -0500

    model3

--- script/app.js
+++ script/app.js
@@ -1,28 +1,55 @@
-let movies = document.querySelector("movies");
-let card = document.getElementsByClassName("videosCard");
-let popular = document.getElementById("popular").value;
-let anime = document.getElementById("anime").value;
-let crime = document.getElementById("crime").value;
+let movies = document.querySelector("#movies");
+let popular = document.getElementById("popular");
+let anime = document.getElementById("anime");
+let crime = document.getElementById("crime");
 
 
-const getMovies = async (url) => {
-  const response = await fetch(url);
-  showMovie.innerHTML = ""
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(movie => {
+const getMoviesCrime = async (url) => {
+  const response = await fetch(url)
+  const data = await response.json()
+  const crimen= data.filter((movie)=> movie.category==="Crimenes")
+  console.log(crimen);
+    crimen.forEach(movie => {
       const { image } = movie
-      showMovie.innerHTML += `
-			<div class='popularMovies'>
-				<img src="${image}" class="..." alt="...">
+      crime.innerHTML += `
+			<div class='crimeMovies'>
+				<img src="${image}" width="300px" class="..." alt="...">
       </div>  
 			`
-      card.appendChild("showMovie")
+      
     })
-  })
+  
 }
-const cardFilled = await request("http://localhost:4000/movies")
+getMoviesCrime('http://localhost:4000/movies')
 
-// getMovies.addEventListener("", () =>
-//   getMovies("http://localhost:4005/movies")
-// );
+
+const getMoviesAnime = async (url) => {
+  const response= await fetch(url)
+  const data= await response.json()
+  const animes= data.filter((movie)=> movie.category==="Animes")
+  console.log(animes);
+    animes.forEach(movie =>{
+      const {image} = movie
+      anime.innerHTML += `
+      <div class= 'animeMovies'>
+        <img src="${image}" width="300px" class="..." alt="...">
+      </div>  `
+    })
+}
+getMoviesAnime('http://localhost:4000/movies')
+
+
+const getMoviesPop = async (url) => {
+  const response= await fetch(url)
+  const data= await response.json()
+  const populares= data.filter((movie)=> movie.category==="Accion"||"Terror")
+  console.log(populares);
+    populares.forEach(movie =>{
+      const {image} = movie
+      popular.innerHTML += `
+      <div class= 'popularMovies'>
+        <img src="${image}" width="300px" class="..." alt="...">
+      </div>  `
+    })
+}
+getMoviesPop('http://localhost:4000/movies')
